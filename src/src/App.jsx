216ef85d949f4fb773e1cf1a2e6b2ec7a19f05e8
alexandrajerez21-design <alexandrajerez21@gmail.com>
2025-12-05 23:43:42 -0300

    Delete src/src directory

--- src/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <h1 style={{ textAlign: "center", marginTop: "50px" }}>
-      Bienvenido a ARstore 🚀  
-      <br/>Tu minimarket digital está vivo 👇
-    </h1>
-  );
-}
