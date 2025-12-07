1138612932968ba5e5a358c9afa2f52589379907
alexandrajerez21-design <alexandrajerez21@gmail.com>
2025-12-07 20:20:13 -0300

    Update productos.js

--- src/data/productos.js
+++ src/data/productos.js
@@ -1,7 +1,17 @@
+// src/data/productos.js
+// Base inicial del inventario ARStore
+
 const productos = [
-  { id: 1, nombre: "Coca Cola 350ml", precio: 1000 },
-  { id: 2, nombre: "Coca Cola 3ltrs", precio: 3000 },
-  { id: 3, nombre: "Pan Hallulla", precio: 1500 },
+  { id: 1, nombre: "Coca-Cola 1.5L", precio: 1200, stock: 10 },
+  { id: 2, nombre: "Sprite 1.5L", precio: 1150, stock: 8 },
+  { id: 3, nombre: "Pepsi 1.5L", precio: 1100, stock: 5 },
+  { id: 4, nombre: "Doritos 90g", precio: 990, stock: 15 },
+  { id: 5, nombre: "Super 8", precio: 350, stock: 30 },
+  { id: 6, nombre: "Pan Hallulla", precio: 150, stock: 40 },
+  { id: 7, nombre: "Aceite 1L", precio: 1800, stock: 12 },
+  { id: 8, nombre: "Azúcar 1kg", precio: 1200, stock: 17 },
+  { id: 9, nombre: "Arroz 1kg", precio: 1100, stock: 20 },
+  { id: 10, nombre: "Fideos Carozzi 400g", precio: 890, stock: 25 }
 ];
 
 export default productos;
