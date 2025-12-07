20ac4c829fa4c2924a900995032d537c85afdf92
alexandrajerez21-design <alexandrajerez21@gmail.com>
2025-12-07 16:19:46 -0300

    Create productos.js

---
+++ src/data/productos.js
@@ -0,0 +1,11 @@
+// 📦 Base inicial de productos del minimarket
+// ⚠ Estos precios y stocks son editables más adelante en el módulo inventario
+
+export const productosBase = [
+  { nombre: "Coca Cola 3lts", precio: 1500, stock: 20 },
+  { nombre: "Pepsi 3lts", precio: 1400, stock: 15 },
+  { nombre: "Fanta 3lts", precio: 1500, stock: 10 },
+  { nombre: "Leche Entera 1L", precio: 1000, stock: 30 },
+  { nombre: "Pan Hallulla kg", precio: 2200, stock: 12 },
+  { nombre: "Arroz 1kg", precio: 1200, stock: 25 },
+];
